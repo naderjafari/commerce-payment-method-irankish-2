b8c5081b77e7672befccab5aa1ce2d8b58237af1
Nader Jafari <nader.jafari1988@gmail.com>
2019-02-28 21:20:55 +0330

    First commit

---
+++ src/main/java/com/sain/commerce/payment/method/saderat/constants/SaderatBankPaymentPortletKeys.java
@@ -0,0 +1,11 @@
+package com.sain.commerce.payment.method.saderat.constants;
+
+/**
+ * @author Nader
+ */
+public class SaderatBankPaymentPortletKeys {
+
+	public static final String SaderatBankPayment = "saderatbankpayment";
+	public static final String SaderatBankPayment1 = "saderatbankpayment  ";
+
+}
---
+++ src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,7 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="saderatbankpayment.caption"/></b>
+	TTT
+	dsds sddfsdfsd fsdfddsdsd
+</p>
